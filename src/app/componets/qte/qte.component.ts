4a4a496d3076db796c618a62acf0f5ab3c8c19df
PhilippOesch <49947694+PhilippOesch@users.noreply.github.com>
2019-10-29 01:14:27 +0100

    QTE Prototyp

--- src/app/componets/qte/qte.component.ts
+++ src/app/componets/qte/qte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, DefaultIterableDiffer} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-qte',
@@ -8,16 +9,32 @@ import { Component, OnInit, Input, DefaultIterableDiffer} from '@angular/core';
 export class QteComponent implements OnInit {
   @Input() decision1='';
   @Input() decision1Link='';
-  @Input() decision2='';
-  @Input() decision2Link='';
   @Input() defaultLink='';
   @Input() timerLength=5;
 
-  constructor() {
+  constructor(private router: Router) {
    }
 
   ngOnInit() {
+    setTimeout(this.redirectToDefault, ((this.timerLength+0.7)*1000));
 
   }
 
+  redirectToDefault = () => {this.router.navigate([this.defaultLink]); };
+
+  getTimerDelay(extraDelay: number){
+    return (this.timerLength+ extraDelay) + 's';
+  }
+
+  getAnimationDur(){
+    return this.timerLength+'s';
+  }
+
+  getQTEBackroundDuration(){
+    return  '0.2s, 1s';
+  }
+
+  getQTEBackroundDelay(){
+    return this.timerLength + 's , ' + (this.timerLength + 0.5) + 's';
+  }
 }
